48465807652a8d19dc6f210be5dc7826d7262f02
kiznlh <84329970+kiznlh@users.noreply.github.com>
2022-03-30 18:47:18 +0700

    clean main()

--- PROJECT COURSES REGISTRATION SYSTEM/PROJECT/MAIN.cpp
+++ PROJECT COURSES REGISTRATION SYSTEM/PROJECT/MAIN.cpp
@@ -8,29 +8,10 @@ int main()
 	//fstream input, output;	
 	//input.open("Student.csv", ios::in); // tạm thời mình bỏ cái này nha
 	//Thư: theo tui thấy thì có lẽ phải đọc file trong vòng while 
-	while (true)
+	while (true) //the condition of this loop will be replaced.
 	{
-		
-		//the condition of this loop will be replaced.
-		int type;
-		//
-		//Input csv file
-		//
-		SignIn *pHead = nullptr;
-		//When log in in the system, is there a need to know what type of user beforehand // Thư: có á
-		//
-		//
-		cout << "COURSES REGISTRATION SYSTEM BY GROUP 7\n";
-		cout << "PLEASE LOG IN\n";
-
-		LogIn(pHead);
-		// Needed a show profile function (student or staff or teacher)
-		// Needed a log out function
-		menuOfGeneral(type);
-		doSomething(menuOfGeneral(type),type,pHead);
-
+		//chờ xong mấy cái login như nào mới biết để sửa trong này
 	}
-	//input.close();
 	return 0;
 	
 }
